26dc428de327298af47a6e2208f1c13ea8f85ac2
Maja <maja.gacesa@gmail.com>
2020-04-24 15:25:14 +0200

    [03] define IsPrime function

--- Problem_03_Primes.py
+++ Problem_03_Primes.py
@@ -3,4 +3,10 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+def IsPrime(Number):
+    for divisor in range(2,Number):
+        if Number%divisor == 0:
+            return False
+    return True
+
 input = 600851475143
