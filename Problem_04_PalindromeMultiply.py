2c0f6c12f21aa66ec5568e3eb09d5fcbc311d0f6
Maja <maja.gacesa@gmail.com>
2020-04-28 12:53:17 +0200

    [04] define IsPalindrome function

--- Problem_04_PalindromeMultiply.py
+++ Problem_04_PalindromeMultiply.py
@@ -3,3 +3,17 @@
 
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+def IsPalindrome(Number):
+    Candidate = str(Number)
+    Digits = len(Candidate)
+    if (Digits > 1):
+        # do some testing
+        for digitcounter in range (0,Digits//2):
+            if (Candidate[digitcounter] != Candidate[-digitcounter-1]):
+                return False
+        return True
+    else:
+        # every single digit number is a palindrome
+        return True
+
