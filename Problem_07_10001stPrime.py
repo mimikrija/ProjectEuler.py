e14a002c5c17be2864de58c43513fbcad450ed12
Maja <maja.gacesa@gmail.com>
2020-04-29 17:09:44 +0200

    [07] optimization: add timer
    total solution time:  40.7781286239624

--- Problem_07_10001stPrime.py
+++ Problem_07_10001stPrime.py
@@ -3,6 +3,7 @@
 
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
+import time
 
 def is_prime(candidate):
     for divisor in range(2,candidate):
@@ -11,10 +12,13 @@ def is_prime(candidate):
     return True
 
 counter = 0
-candidate = 1
+solution = 1
+start = time.time()
 while counter < 10001:
-    candidate +=1
-    if is_prime(candidate):
+    solution +=1
+    if is_prime(solution):
         counter += 1
+end = time.time()
 
-print (counter, "-th prime is ", candidate )
+print (counter, "-th prime is ", solution )
+print ("total solution time: ", end-start)
