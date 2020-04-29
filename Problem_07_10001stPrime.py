46052f1c84bbbf60d4550593b0e46434a3ded3cb
Maja <maja.gacesa@gmail.com>
2020-04-29 16:43:23 +0200

    [07] copy is_prime from problem 03

--- Problem_07_10001stPrime.py
+++ Problem_07_10001stPrime.py
@@ -3,3 +3,9 @@
 
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
+
+def is_prime(candidate):
+    for divisor in range(2,candidate):
+        if candidate%divisor == 0:
+            return False
+    return True
