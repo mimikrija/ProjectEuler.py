c77194410abb718f7786dd27bdab44e4521f6918
Maja <maja.gacesa@gmail.com>
2020-04-29 11:21:11 +0200

    [05] define evenly_divisible function

--- Problem_05_SmallestMultiple.py
+++ Problem_05_SmallestMultiple.py
@@ -3,3 +3,10 @@
 
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def evenly_divisible(number,max_divisor):
+    for divisor in range (2,max_divisor+1):
+        if number%divisor != 0:
+            return False
+    return True
+
