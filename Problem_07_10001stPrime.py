a5f5f5e448a8064ba1d54e5448074a266c0abd81
Maja <maja.gacesa@gmail.com>
2020-04-30 11:54:20 +0200

    [07] check for divisibility up to the square root
    and some other lower prime checks which don't really bring any speed up.
    but the root one gives significant speed up:
    total solution time:  0.2288682460784912

--- Problem_07_10001stPrime.py
+++ Problem_07_10001stPrime.py
@@ -4,9 +4,20 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 import time
+import math
 
 def is_prime(candidate):
-    for divisor in range(3,candidate//2):
+    if candidate == 1:
+        return False # 1 is not a prime
+    if candidate < 4:
+        return True # but all other numbers less than 4 are
+    if candidate%2 == 0:
+        return False # all other primes are odd (except 2 which is handle by the above case)
+    if candidate < 9:
+        return True # which leaves us with 5 and 7
+    for divisor in range(3,math.floor(math.sqrt(candidate))+1):
+        # divisibility by 2 was already eliminated above,
+        # we can only check up until sqrt(candidate)+1
         if candidate%divisor == 0:
             return False
     return True
