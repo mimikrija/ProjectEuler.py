1afdb8fc6f30e94ad52fad6fe2188f47578fb037
Maja <maja.gacesa@gmail.com>
2020-05-01 21:01:52 +0200

    [09] straightforward solution?

--- Problem_09_PithagoraTriplet.py
+++ Problem_09_PithagoraTriplet.py
@@ -9,3 +9,18 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+
+import math
+from math import floor, sqrt
+
+def SpecialTriplet():
+    for a in range(1,1000):
+        for b in range (1,1000):
+            potential_c_square = a**2 + b**2
+            if floor(sqrt(potential_c_square)) == sqrt(potential_c_square):
+                c = int(floor(sqrt(potential_c_square)))
+                if a + b + c == 1000:
+                    print ("Solution is: ", a ,"x", b, "x",c, " = ", a*b*c)
+                    return
+
+SpecialTriplet()
