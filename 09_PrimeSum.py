60ac8960fd6a21dd5e7f5969bf3592af9544a37a
Maja <maja.gacesa@gmail.com>
2020-05-03 16:53:37 +0200

    [09] borrow is_prime function from problem 07

--- 09_PrimeSum.py
+++ 09_PrimeSum.py
@@ -3,3 +3,22 @@
 
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # # Find the sum of all the primes below two million.
+
+import math
+
+def is_prime(candidate):
+    if candidate == 1:
+        return False # 1 is not a prime
+    if candidate < 4:
+        return True # but all other numbers less than 4 are
+    if candidate%2 == 0:
+        return False # all other primes are odd (except 2 which is handle by the above case)
+    if candidate < 9:
+        return True # which leaves us with 5 and 7
+    for divisor in range(3,math.floor(math.sqrt(candidate))+1):
+        # divisibility by 2 was already eliminated above,
+        # we can only check up until sqrt(candidate)+1
+        if candidate%divisor == 0:
+            return False
+    return True
+    
