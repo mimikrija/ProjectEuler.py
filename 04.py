bfcbb1bf730e5a34597a1c23d01aa0af1cf9f4ab
Maja <maja.gacesa@gmail.com>
2021-04-27 16:00:25 +0200

    [04] improve efficiency by only looking in the top
    there is no need to go all the way to the lowest numbers in a given
    digit range because there will always be a palindrome in the range as
    defined here.

--- 04.py
+++ 04.py
@@ -3,19 +3,22 @@
 
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
-
-def IsPalindrome(Number):
-    Candidate = str(Number)
-    return Candidate == Candidate[::-1]
-
-Solution = 1
-for FirstNumber in range (999,99,-1):
-    for SecondNumber in range (FirstNumber,99,-1):
-        Prospect = FirstNumber*SecondNumber
-        if Prospect > Solution and IsPalindrome(Prospect):
-            Solution = Prospect
-            FactorOne = FirstNumber
-            FactorTwo = SecondNumber
+from itertools import combinations
+from operator import mul
 
 
-print ("The largest palindrome made from the product of two 3-digit numbers is: ", FactorOne, " x ", FactorTwo, " = ", Solution)
+def is_palindrome(num):
+    candidate = str(num)
+    return candidate == candidate[::-1]
+
+def largest_palindrome_which_is_a_two_number_product(digits):
+    top = 10**digits - 1
+    bottom = top - top // 10**(digits-3) if digits >= 3 else 10
+    products = ((mul(*p), *p) for p in combinations(range(top, bottom, -1), 2) if is_palindrome(mul(*p)))
+    return max(products)
+
+digits = 3
+max_palindrome, factor_1, factor_2 = largest_palindrome_which_is_a_two_number_product(digits)
+
+
+print(f"The largest palindrome made from the product of two {digits}-digit numbers is: {factor_1} x {factor_2} = {max_palindrome}!")
