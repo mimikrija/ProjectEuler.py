3aebc92c3ca81f995df99268b1827d382534576e
Maja <maja.gacesa@gmail.com>
2021-05-01 14:09:36 +0200

    [07] prime generator + islice

--- 07.py
+++ 07.py
@@ -3,33 +3,27 @@
 
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
-import time
-import math
 
-def is_prime(candidate):
-    if candidate == 1:
-        return False # 1 is not a prime
-    if candidate < 4:
-        return True # but all other numbers less than 4 are
-    if candidate%2 == 0:
-        return False # all other primes are odd (except 2 which is handle by the above case)
-    if candidate < 9:
-        return True # which leaves us with 5 and 7
-    for divisor in range(3,math.floor(math.sqrt(candidate))+1):
-        # divisibility by 2 was already eliminated above,
-        # we can only check up until sqrt(candidate)+1
-        if candidate%divisor == 0:
-            return False
-    return True
+from math import floor, sqrt
+from itertools import islice
 
-counter = 1
-solution = 1
-start = time.time()
-while counter < 10001:
-    solution +=2
-    if is_prime(solution):
-        counter += 1
-end = time.time()
 
-print (counter, "-th prime is ", solution )
-print ("total solution time: ", end-start)
+def prime_generator():
+    for prime in (2, 3, 5, 7):
+        yield prime
+
+    while True:
+        prime += 2
+        if any(prime % divisor == 0 for divisor in range(3, floor(sqrt(prime) + 1))):
+            pass
+        else:
+            yield prime
+
+def prime(count):
+    pr = prime_generator()
+    return next(islice(pr, count-1, count))
+
+
+n = 10001
+print(f'{n}-th prime is {prime(n)}!')
+# 104743
