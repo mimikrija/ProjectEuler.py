b7ded5d883d78b56bfbe6fa87d14566fd7cc67f1
Maja <maja.gacesa@gmail.com>
2021-05-01 11:30:25 +0200

    [06] replace for loops with sum + comprehension

--- 06.py
+++ 06.py
@@ -3,13 +3,8 @@
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-sum_of_the_squares = 0
-natural_sum = 0
+sum_of_the_squares = sum(natural**2 for natural in range(1, 101))
+square_of_the_sum = sum(natural for natural in range(1, 101))**2
 
-for natural in range(1,101):
-    sum_of_the_squares += natural**2
-    natural_sum += natural
-
-square_of_the_sum = natural_sum**2
 print(f"Solution is: {square_of_the_sum - sum_of_the_squares}")
 # 25164150
