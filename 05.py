2cb6de8474df8f93949866f429b845e16ff59bd2
Maja <maja.gacesa@gmail.com>
2021-05-01 11:23:41 +0200

    [05] prime check was redundant

--- 05.py
+++ 05.py
@@ -7,24 +7,21 @@
 from math import sqrt
 
 
-def is_prime(num):
-    return not any(num % divisor == 0 for divisor in range(2, int(sqrt(num)+1)))
-
-def min_prime_factor(num):
+def min_factor(num):
     for n in range(2, num+1):
-        if num % n == 0 and is_prime(n):
+        if num % n == 0:
             return n
 
-def min_missing_prime_divisor(solution, num):
+def min_missing_divisor(solution, num):
     for n in range(2, num+1):
         if solution % n != 0:
-            return min_prime_factor(n)
+            return min_factor(n)
 
 def smallest_multiple(num):
     "keeps multiplying solution with a missing divisor until it is evenly divisible by all numbers up to `num` including"
     solution = 1
     while True:
-        missing = min_missing_prime_divisor(solution, num)
+        missing = min_missing_divisor(solution, num)
         if not missing:
             return solution
         else:
