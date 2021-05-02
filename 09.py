dccdf6d57d2bb9332a493f25493ae8ca5ceeaf9b
Maja <maja.gacesa@gmail.com>
2021-05-02 16:31:49 +0200

    [09] pythonize a bit
    use combinations to generate all possible triplets whose sum is 1000,
    then look for the one triplet which is a pythagora triplet.

--- 09.py
+++ 09.py
@@ -9,18 +9,22 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+from itertools import combinations
+from functools import reduce
+from operator import mul
 
-import math
-from math import floor, sqrt
+is_pyth_tripplet = lambda a, b, c: a**2 + b**2 == c**2
 
-def SpecialTriplet():
-    for a in range(1,1000):
-        for b in range (1,1000):
-            potential_c_square = a**2 + b**2
-            if floor(sqrt(potential_c_square)) == sqrt(potential_c_square):
-                c = int(floor(sqrt(potential_c_square)))
-                if a + b + c == 1000:
-                    print ("Solution is: ", a ,"x", b, "x",c, " = ", a*b*c)
-                    return
 
-SpecialTriplet()
+GOAL_SUM = 1000
+
+# set of sorted triplets which give GOAL_SUM
+triplets_sum_goal = {tuple(sorted((*combo, GOAL_SUM - sum(combo))))
+                        for combo in combinations(range(1, GOAL_SUM-1), 2)
+                        if sum(combo) < GOAL_SUM}
+
+
+triplet = next(combo for combo in triplets_sum_goal if is_pyth_tripplet(*combo))
+sides = ' x '.join(str(side) for side in triplet)
+print(f'pythagora triplet with sum of sides equal to {GOAL_SUM} is: {sides} = {reduce(mul, triplet)}')
+# 31875000
