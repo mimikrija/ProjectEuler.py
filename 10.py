aafc07f8e9d0c141f2bbb6522c551a57e1ccccb3
Maja <maja.gacesa@gmail.com>
2021-05-02 17:10:17 +0200

    [10] attempt to solve with prime generator - slow
    because we have to perform so many divisions, this approach is obviously
    not optimal

--- 10.py
+++ 10.py
@@ -4,43 +4,27 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # # Find the sum of all the primes below two million.
 
-import math
-import time
+from math import floor, sqrt
+from itertools import islice
+from time import time
 
-def is_prime(candidate):
-    if candidate == 1:
-        return False # 1 is not a prime
-    if candidate < 4:
-        return True # but all other numbers less than 4 are
-    if candidate%2 == 0:
-        return False # all other primes are odd (except 2 which is handle by the above case)
-    if candidate < 9:
-        return True # which leaves us with 5 and 7
-    for divisor in range(3,math.floor(math.sqrt(candidate))+1):
-        # divisibility by 2 was already eliminated above,
-        # we can only check up until sqrt(candidate)+1
-        if candidate%divisor == 0:
-            return False
-    return True
 
-start = time.time()
-solution = 5
-potential_prime = 4
-k = 1
-switch_k = False
-while potential_prime <= 2000000:
-    if is_prime(potential_prime):
-        solution += potential_prime
-    if not switch_k:
-        potential_prime = 6*k - 1
-    else:
-        potential_prime = 6*k + 1
-        k += 1
-    
-    switch_k = not switch_k
+def prime_generator(limit):
+    for prime in (2, 3, 5, 7):
+        yield prime
 
-        
-end = time.time()
+    while prime < limit:
+        prime += 2
+        if any(prime % divisor == 0 for divisor in range(3, floor(sqrt(prime) + 1))):
+            pass
+        else:
+            yield prime
 
-print("sum of all primes below two million is: ", solution)
-print ("total solution time: ", end-start)
+UNDER = 2 * 1000 * 1000
+pr = prime_generator(UNDER)
+a = time()
+solution = sum(prime for prime in islice(pr, None))
+b = time()
+print(f'sum of primes smaller than {UNDER} is {solution} and it takes {b-a} seconds to solve it.')
+# 142913828922, ~20 s
+
